b7e0eb96d98931e9cfe977d448df466ffcc9e7f9
Jonathan Chang <jonathan.chang@silverpond.com.au>
2012-02-17 08:44:24 +1100

    updated connection spec to connect to riak solr instance

--- spec/api/connection_spec.rb
+++ spec/api/connection_spec.rb
@@ -3,7 +3,7 @@ describe "RSolr::Connection" do
   
   context "setup_raw_request" do
     c = RSolr::Connection.new
-    base_url = "http://localhost:8983/solr"
+    base_url = "http://localhost:8098/solr"
     client = RSolr::Client.new c, :url => base_url
     req = c.send :setup_raw_request, {:headers => {"content-type" => "text/xml"}, :method => :get, :uri => URI.parse(base_url + "/select?q=*:*")}
     req.path.should == "/solr/select?q=*:*"
@@ -18,13 +18,13 @@ describe "RSolr::Connection" do
     subject { RSolr::Connection.new } 
 
     it "should configure Net:HTTP read_timeout" do
-      subject.execute client, {:uri => URI.parse("http://localhost/some_uri"), :method => :get, :read_timeout => 42}
+      subject.execute client, {:uri => URI.parse("http://localhost:8098/some_uri"), :method => :get, :read_timeout => 42}
       http = subject.instance_variable_get(:@http)
       http.read_timeout.should == 42
     end
 
     it "should use Net:HTTP default read_timeout if not specified" do
-      subject.execute client, {:uri => URI.parse("http://localhost/some_uri"), :method => :get}
+      subject.execute client, {:uri => URI.parse("http://localhost:8098/some_uri"), :method => :get}
       http = subject.instance_variable_get(:@http)
       http.read_timeout.should == 60
     end
@@ -36,13 +36,13 @@ describe "RSolr::Connection" do
     subject { RSolr::Connection.new } 
 
     it "should configure Net:HTTP open_timeout" do
-      subject.execute client, {:uri => URI.parse("http://localhost/some_uri"), :method => :get, :open_timeout => 42}
+      subject.execute client, {:uri => URI.parse("http://localhost:8098/some_uri"), :method => :get, :open_timeout => 42}
       http = subject.instance_variable_get(:@http)
       http.open_timeout.should == 42
     end
 
     it "should use Net:HTTP default open_timeout if not specified" do
-      subject.execute client, {:uri => URI.parse("http://localhost/some_uri"), :method => :get}
+      subject.execute client, {:uri => URI.parse("http://localhost:8098/some_uri"), :method => :get}
       http = subject.instance_variable_get(:@http)
       http.open_timeout.should == nil
     end
